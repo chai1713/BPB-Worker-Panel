6fa8bb6baec926eac56b1799c54230f65986cc56
chai1713 <155163188+chai1713@users.noreply.github.com>
2024-07-20 14:57:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -200,7 +200,7 @@ export default {
 		
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'ip.sb';
+                        url.hostname = 'www.whoer.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
