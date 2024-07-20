0472b1fb420e34b3a8682ba011a9394ec563e237
chai1713 <155163188+chai1713@users.noreply.github.com>
2024-07-20 14:44:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -197,7 +197,13 @@ export default {
                                 'Content-Type': 'text/plain',
                             }
                         });
-
+		
+                    default:
+                        // return new Response('Not found', { status: 404 });
+                        url.hostname = 'ip.sb';
+                        url.protocol = 'https:';
+                        request = new Request(url, request);
+                        return await fetch(request);
                 }
             } else {
                 return await vlessOverWSHandler(request);
